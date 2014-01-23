f491c1160897228acff57aea4f05f04c199bcd55
Filip Hájek <Epick@Filip-MacBook-Pro.local>
2014-01-23 13:44:49 -0600

    Sun

--- js/earth.js
+++ js/earth.js
@@ -1,91 +1,164 @@
 // Created by Bjorn Sandvik - thematicmapping.org
-(function () {
+var width  = window.innerWidth,
+	height = window.innerHeight;
 
-	var webglEl = document.getElementById('webgl');
+// Earth params
+var radius   = 0.5,
+	segments = 64,
+	rotation = 6,
+	coordinates = [10, 0, 10];  
 
+var gradientCanvas;
+var gradientImage;
+
+var webglEl = document.getElementById('webgl');
+
+function start(e) {
 	if (!Detector.webgl) {
 		Detector.addGetWebGLMessage(webglEl);
 		return;
 	}
 
-	var width  = window.innerWidth,
-		height = window.innerHeight;
+	gradientImage = document.createElement('img');
+	gradientImage.onload = postStarGradientLoaded;
+	gradientImage.src = 'images/star_color_modified.png';	
+}
 
-	// Earth params
-	var radius   = 0.5,
-		segments = 32,
-		rotation = 6;  
+function postStarGradientLoaded() {
+	gradientCanvas = document.createElement('canvas');
+	gradientCanvas.width = gradientImage.width;
+	gradientCanvas.height = gradientImage.height;
+	gradientCanvas.getContext('2d').drawImage( gradientImage, 0, 0, gradientImage.width, gradientImage.height );
+	gradientCanvas.getColor = function( percentage ){
+		return this.getContext('2d').getImageData(0, 1, 1, 1).data;
+	}
 
-	var scene = new THREE.Scene();
 
-	var camera = new THREE.PerspectiveCamera(45, width / height, 0.01, 1000);
-	camera.position.z = 1.5;
+	loadShaders( shaderList, function(e){
+		//	we have the shaders loaded now...
+		shaderList = e;
 
-	var renderer = new THREE.WebGLRenderer();
+		initWorld();
+	});	
+}
+
+function initWorld() {
+	scene = new THREE.Scene();
+
+	camera = new THREE.PerspectiveCamera(45, width / height, 0.01, 1000);
+	camera.position.set(coordinates[0] + 1.5, coordinates[1] + 1, coordinates[2] + 1.5);
+
+	renderer = new THREE.WebGLRenderer({antialias: true});
 	renderer.setSize(width, height);
 
 	scene.add(new THREE.AmbientLight(0x333333));
 
-	var light = new THREE.DirectionalLight(0xffffff, 1);
-	light.position.set(5,3,5);
-	scene.add(light);
+	star = addStarToScene(.5, 32, [0, 0, 0], null);
+	//scene.add(star)
 
-    var sphere = createSphere(radius, segments);
-	sphere.rotation.y = rotation; 
-	scene.add(sphere)
+	planet = addPlanetToScene(radius, segments, coordinates, true);
+	scene.add(planet)
 
-    var clouds = createClouds(radius, segments);
-	clouds.rotation.y = rotation;
-	scene.add(clouds)
-
-	var stars = createStars(90, 64);
+	stars = createStars(400, 64);
 	scene.add(stars);
 
-	var controls = new THREE.TrackballControls(camera);
+	sun = makeSun(
+	 	{
+	 		radius: 2,
+	 		spectral: 1
+	 	}
+	 );
+	scene.add(sun);
+	sun.position.set(0, 0, 0);
+
+	axisHelper = new THREE.AxisHelper( 8 );
+	scene.add( axisHelper );
+
+	controls = new THREE.TrackballControls(camera);
+	controls.target.set(coordinates[0], coordinates[1], coordinates[2]);
 
 	webglEl.appendChild(renderer.domElement);
 
 	render();
+}
 
-	function render() {
-		controls.update();
-		sphere.rotation.y += 0.0005;
-		clouds.rotation.y += 0.0005;		
-		requestAnimationFrame(render);
-		renderer.render(scene, camera);
+function render() {
+	renderer.clear();
+
+	controls.update();
+	planet.rotation.y += 0.0005;	
+	requestAnimationFrame(render);
+	renderer.render(scene, camera);
+}
+
+function createStar(radius, segments) {
+	return new THREE.Mesh(
+		new THREE.SphereGeometry(radius, segments, segments),
+		new THREE.MeshPhongMaterial({
+			map:         THREE.ImageUtils.loadTexture('images/sunmap.jpg')				
+		})
+	);		
+}
+
+function addStarToScene(radius, segments, position, luminosity) {
+	var star = new THREE.Object3D();
+
+	var light = new THREE.PointLight(0xffffff, 1, 200);
+	star.add(light)
+
+	var object = createStar(radius, segments);
+	star.add(object);
+	star.position.set(position[0], position[1], position[2]);
+
+	return star;
+}
+
+function createPlanet(radius, segments) {
+	return new THREE.Mesh(
+		new THREE.SphereGeometry(radius, segments, segments),
+		new THREE.MeshPhongMaterial({
+			map:         THREE.ImageUtils.loadTexture('images/2_no_clouds_4k.jpg'),
+			bumpMap:     THREE.ImageUtils.loadTexture('images/elev_bump_4k.jpg'),
+			bumpScale:   0.01,
+			specularMap: THREE.ImageUtils.loadTexture('images/water_4k.png'),
+			specular:    new THREE.Color('grey')								
+		})
+	);
+}
+
+function createClouds(radius, segments) {
+	return new THREE.Mesh(
+		new THREE.SphereGeometry(radius + 0.003, segments, segments),			
+		new THREE.MeshPhongMaterial({
+			map:         THREE.ImageUtils.loadTexture('images/fair_clouds_4k.png'),
+			transparent: true
+		})
+	);		
+}
+
+function addPlanetToScene(radius, segments, position, withclouds) {
+	var planet = new THREE.Object3D();
+
+    var object = createPlanet(radius, segments);
+	object.rotation.y = rotation; 
+	planet.add(object)
+
+	if(withclouds === true) {
+	    var clouds = createClouds(radius, segments);
+		clouds.rotation.y = rotation;
+		planet.add(clouds)
 	}
 
-	function createSphere(radius, segments) {
-		return new THREE.Mesh(
-			new THREE.SphereGeometry(radius, segments, segments),
-			new THREE.MeshPhongMaterial({
-				map:         THREE.ImageUtils.loadTexture('images/2_no_clouds_4k.jpg'),
-				bumpMap:     THREE.ImageUtils.loadTexture('images/elev_bump_4k.jpg'),
-				bumpScale:   0.005,
-				specularMap: THREE.ImageUtils.loadTexture('images/water_4k.png'),
-				specular:    new THREE.Color('grey')								
-			})
-		);
-	}
+	planet.position.set(position[0], position[1], position[2]);
+	return planet;
+}
 
-	function createClouds(radius, segments) {
-		return new THREE.Mesh(
-			new THREE.SphereGeometry(radius + 0.003, segments, segments),			
-			new THREE.MeshPhongMaterial({
-				map:         THREE.ImageUtils.loadTexture('images/fair_clouds_4k.png'),
-				transparent: true
-			})
-		);		
-	}
-
-	function createStars(radius, segments) {
-		return new THREE.Mesh(
-			new THREE.SphereGeometry(radius, segments, segments), 
-			new THREE.MeshBasicMaterial({
-				map:  THREE.ImageUtils.loadTexture('images/galaxy_starfield.png'), 
-				side: THREE.BackSide
-			})
-		);
-	}
-
-}());
+function createStars(radius, segments) {
+	return new THREE.Mesh(
+		new THREE.SphereGeometry(radius, segments, segments), 
+		new THREE.MeshBasicMaterial({
+			map:  THREE.ImageUtils.loadTexture('images/galaxy_starfield.png'), 
+			side: THREE.BackSide
+		})
+	);
+}
---
+++ js/util.js
@@ -0,0 +1,143 @@
+function toTHREEColor( colorString ){
+	return new THREE.Color( parseInt( colorString.substr(1) , 16)  );
+}
+
+var monthNames = new Array(12);
+monthNames[0] = "January";
+monthNames[1] = "February";
+monthNames[2] = "March";
+monthNames[3] = "April";
+monthNames[4] = "May";
+monthNames[5] = "June";
+monthNames[6] = "July";
+monthNames[7] = "August";
+monthNames[8] = "September";
+monthNames[9] = "October";
+monthNames[10] = "November";
+monthNames[11] = "December";			
+
+function toMonthName( monthNumber ){
+	return monthNames[monthNumber];
+}	
+
+function componentToHex(c) {
+	var hex = c.toString(16);
+	return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+	return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function gup( name )
+{
+	name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+	var regexS = "[\\?&]"+name+"=([^&#]*)";
+	var regex = new RegExp( regexS );
+	var results = regex.exec( window.location.href );
+	if( results == null )
+	return "";
+	else
+	return results[1];
+}
+
+function wrap(value, min, rangeSize) {
+	rangeSize-=min;
+    while (value < min) {
+    	value += rangeSize;
+	}
+	return value % rangeSize;
+}
+
+THREE.Curve.Utils.createLineGeometry = function( points ) {
+	var geometry = new THREE.Geometry();
+	for( var i = 0; i < points.length; i ++ ) {
+		geometry.vertices.push( points[i] );
+	}
+	return geometry;
+};
+
+function getAbsOrigin( object3D ){
+	var mat = object3D.matrixWorld;
+	var worldpos = new THREE.Vector3();
+	worldpos.x = mat.n14;
+	worldpos.y = mat.n24;
+	worldpos.z = mat.n34;
+	return worldpos;
+}
+
+var projector = new THREE.Projector();
+function screenXY(object){	
+  var vector = projector.projectVector(new THREE.Vector3().getPositionFromMatrix(object.matrixWorld), camera);  
+	var result = new Object();
+    var windowWidth = window.innerWidth;
+    var minWidth = 1280;
+    if(windowWidth < minWidth) {
+        windowWidth = minWidth;
+    }
+	result.x = Math.round( vector.x * (windowWidth/2) ) + windowWidth/2;
+	result.y = Math.round( (0-vector.y) * (window.innerHeight/2) ) + window.innerHeight/2;
+	return result;
+}	
+
+function buildHexColumnGeo(rad, height){
+	var points = [];
+	var ang = 0;
+	var sixth = 2*Math.PI / 6;
+	for(var i=0; i<7; i++){					
+		var x = Math.cos(ang) * rad;
+		var y = -Math.sin(ang) * rad;
+		points.push( new THREE.Vector2(x,y) );
+		ang += sixth;
+	}
+	var shape = new THREE.Shape(points);
+
+	var options = {
+		size: 			0,
+		amount: 		height,
+		steps: 			1,
+		bevelEnabled:  	false,
+	};
+	var extrudedGeo = new THREE.ExtrudeGeometry(shape, options);
+	return extrudedGeo;	    	
+}
+
+function map(v, i1, i2, o1, o2) {
+   return o1 + (o2 - o1) * (v - i1) / (i2 - i1);
+ }
+
+ function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function roundNumber(num, dec) {
+	var result = Math.round(num*Math.pow(10,dec))/Math.pow(10,dec);
+	return result;
+}
+
+function wrap(value, min, rangeSize) {
+  rangeSize-=min;
+    while (value < min) {
+      value += rangeSize;
+  }
+  return value % rangeSize;
+}
+
+function constrain(v, min, max){
+  if( v < min )
+    v = min;
+  else
+  if( v > max )
+    v = max;
+  return v;
+}
+
+function random(low, high) {
+  if (low >= high) return low;
+  var diff = high - low;
+  return (Math.random() * diff) + low;
+}
+
+function map( value, istart, istop, ostart, ostop) {
+  return ostart + (ostop - ostart) * ((value - istart) / (istop - istart));
+}
