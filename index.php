1d3b8d23c738b07ca62fc5dbf04d041ee7823ec6
Filip Hájek <Epick@Filip-MacBook-Pro.local>
2014-02-03 15:06:20 -0600

    Revert 9305f12..37d05f8
    This rolls back to commit 9305f12392c0c60599a630a51b85fc191acad798.

--- index.php
+++ index.php
@@ -3,45 +3,38 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Space MMO - Game</title>
-	    <link rel="stylesheet" type="text/css" href="game/css/gumby.css" />
+		<title>Space MMO</title>
+	    <link rel="stylesheet" type="text/css" href="game/css/bootstrap.min.css" />
+	    <link rel="stylesheet" type="text/css" href="game/css/bootstrap-theme.min.css" />
+		<style>
+			body { margin: 0; overflow: hidden; background-color: #000; }
+			.webgl-error { font: 15px/30px monospace; text-align: center; color: #fff; margin: 50px; }
+			.webgl-error a { color: #fff; }
+	    </style>
 	</head>
-	<body oncontextmenu="return false">
-		<div gumby-fixed="top" gumby-pin="500">My fixed content</div>
-		<div class="wrapper">
-			<div class="row" style="height:5000px;">
-				<div class="three columns">
-					dawinodwnwa 
-					adw 
-					wda daw
-					dw awd 
-					aw
-					 fwaf aw faw
-					  fa
-					   f
-					   fwawfjafjawfbjabwfof 
-					   awf
-					   w af aw
-					   fw afwafaw
-					    awfawfawfawf
-					    afwawfawf
-					    awf
-					    wa
-					    f
-					    awgawgaw
-					    d
-					    wa
-					    fa
-					    w
-					    af
-					    aw
-				</div>
-				<div class="nine columns">
-					AWDNIONWDAIONWAOINOIWNDAO
-				</div>
-			</div>
-		</div>
+	<body onload="start()" oncontextmenu="return false">
+		<div id="webgl"></div>
 		<script src="game/js/jquery-2.0.3.min.js"></script>
-		<script src="game/js/gumby.min.js"></script>
+		<script src="game/js/three.min.js"></script>
+		<script src="game/js/physi.js"></script>
+		<script src="game/js/OBJLoader.js"></script>
+		<script src="game/js/MTLLoader.js"></script>
+		<script src="game/js/OBJMTLLoader.js"></script>
+		<script src="game/js/stats.min.js"></script>
+		<script src="game/js/util.js"></script>
+		<script src="game/js/spacehelpers.js"></script>		
+		<script src="game/js/Detector.js"></script>		
+		<script src="game/js/PlayerControls.js"></script>
+		<script src="game/js/shaderlist.js"></script>  
+		<script src="game/js/lensflare.js"></script>	
+		<script src="game/js/background.js"></script> 		  		
+		<script src="game/js/sun.js"></script> 		
+		<script src="game/js/solarsystem.js"></script>
+		<script src="game/js/ship.js"></script>
+		<script src="game/js/entity.js"></script>
+		<script src="game/js/viewMode.js"></script>
+
+		<script src="main.js"></script>  
+		<script type="text/javascript" src="server/node_modules/socket.io/node_modules/socket.io-client/dist/socket.io.min.js"></script>
 	</body>
 </html>
