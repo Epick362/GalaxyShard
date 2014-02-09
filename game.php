055407bb3b3eb91b11f418a23f4de3d00c7f7190
Filip Hájek <Epick@Filip-MacBook-Pro.local>
2014-02-09 16:02:24 -0600

    Fix Gamename

--- game.php
+++ game.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Space MMO</title>
+		<title>GalaxyShard</title>
 	    <link rel="stylesheet" type="text/css" href="website/css/bootstrap.min.css" />
 	    <link rel="stylesheet" type="text/css" href="website/css/bootstrap-theme.min.css" />
 		<style>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-		<title>GalaxyShards</title>
+		<title>GalaxyShard</title>
 		<link rel="stylesheet" type="text/css" href="website/css/bootstrap.min.css" />
 		<link rel="stylesheet" type="text/css" href="website/css/carousel.css" />
 		<link rel="stylesheet" type="text/css" href="website/css/custom.css" />
