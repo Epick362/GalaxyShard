277d4a58fbdbd3c547d9bf6ee139090fe984055d
Filip Hájek <Epick@Filip-MacBook-Pro.local>
2014-02-17 12:06:16 -0600

    Fix Player Movement

--- game/core/view.js
+++ game/core/view.js
@@ -31,41 +31,6 @@ Initialize = function(mode, data) {
 		objectContainer.add(env.StarDebris())
 		objectContainer.add(env.SolarSystem())
 
-		player = {};
-		player.name = prompt('enter name');
-
-		// Actual Ship
-		shipContainer = new THREE.Object3D();
-		//ship.position.set(0, 3, 0);
-
-		bounding = new Physijs.SphereMesh(
-			new THREE.SphereGeometry(.01, 64, 64),
-			Physijs.createMaterial(
-				new THREE.Material({
-					opacity: 0
-				}),
-				1.0, // high friction
-				0.0 // low restitution
-			),
-			0.1
-		);
-
-		socket.emit('connect', {'name': player.name});
-		socket.on('connected', function(data) {
-			ship = new Ship(data, player.name, data.ship);
-
-			ship.loadModel(function(object3d) {
-				shipContainer.add(object3d)
-			});
-
-			bounding.position.set(data.x, data.y, data.z);
-			bounding.add(shipContainer);
-			bounding.name = player.name+"\'s Ship";
-			
-			objectContainer.add(bounding)
-			bounding.setAngularFactor(new THREE.Vector3(0, 0, 0));
-			socket.emit('fetch.players');
-		});	
 
 		objectContainer.name = "Solar system";
 		return objectContainer;
